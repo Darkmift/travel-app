1606d899458271ba316596e24b4107fc573ff1cc
Darkmift <darkmift@gmail.com>
2023-09-24 05:54:47 +0300

    [main] added coverage

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -4,14 +4,13 @@ import { getRepositoryToken } from '@nestjs/typeorm';
 import { User } from 'src/entities/user.entity';
 import { JwtService } from '@nestjs/jwt';
 import { Logger } from '@nestjs/common';
+import * as bcrypt from 'bcryptjs';
 
-// Mock for the User repository
 const mockUserRepository = {
   findOne: jest.fn(),
   save: jest.fn(),
 };
 
-// Mock for the JwtService
 const mockJwtService = {
   signAsync: jest.fn(),
 };
@@ -42,5 +41,69 @@ describe('AuthService', () => {
     expect(service).toBeDefined();
   });
 
-  // Add more test cases here to cover the methods in AuthService
+  describe('login', () => {
+    it('should return a user and access token if valid credentials are provided', async () => {
+      const mockUser = new User();
+      mockUser.id = 1;
+      mockUser.email = 'test@test.com';
+      mockUser.password = await bcrypt.hash('password', 10);
+
+      mockUserRepository.findOne.mockResolvedValue(mockUser);
+      mockJwtService.signAsync.mockResolvedValue('some-access-token');
+
+      const result = await service.login({
+        email: 'test@test.com',
+        password: 'password',
+      });
+
+      expect(result).toBeDefined();
+      expect(result.access_token).toEqual('some-access-token');
+    });
+
+    it('should throw UnauthorizedException if user is not found', async () => {
+      mockUserRepository.findOne.mockResolvedValue(null);
+
+      await expect(
+        service.login({ email: 'test@test.com', password: 'password' }),
+      ).rejects.toThrow('Invalid email or password.');
+    });
+
+    it('should throw UnauthorizedException if password is incorrect', async () => {
+      const mockUser = new User();
+      mockUser.id = 1;
+      mockUser.email = 'test@test.com';
+      mockUser.password = await bcrypt.hash('wrong-password', 10);
+
+      mockUserRepository.findOne.mockResolvedValue(mockUser);
+
+      await expect(
+        service.login({ email: 'test@test.com', password: 'password' }),
+      ).rejects.toThrow('Invalid email or password.');
+    });
+  });
+
+  describe('validateUser', () => {
+    // Add your test cases for validateUser here
+  });
+
+  describe('register', () => {
+    it('should successfully register a user', async () => {
+      const mockUser = new User();
+      mockUser.email = 'test@test.com';
+      mockUser.password = 'password';
+
+      mockUserRepository.save.mockResolvedValue(mockUser);
+
+      const result = await service.register(mockUser);
+
+      expect(result).toBeUndefined();
+      expect(mockUserRepository.save).toHaveBeenCalledWith(
+        expect.objectContaining({
+          email: 'test@test.com',
+        }),
+      );
+    });
+
+    // Add more test cases for failure scenarios, validation, etc.
+  });
 });

3d7ea712bc83de0afadb7137113ce8df1acf037d
Darkmift <darkmift@gmail.com>
2023-09-25 13:56:55 +0300

    [main] config changes

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "src/styles/variables.scss";`,
+      },
+    },
+  },
+  define: {
+    'process.env.API_URL': JSON.stringify('https://api.example.com'),
+  },
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3001',
+    },
+  },
+  build: {
+    outDir: 'dist',
+    minify: 'terser',
+  },
+});

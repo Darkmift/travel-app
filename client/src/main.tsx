8804568bbe957d25f00efba6d0a246f8968a3bb8
Darkmift <darkmift@gmail.com>
2023-09-25 22:40:30 +0300

    [main] added devtools

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import MainAppRouter from './layout/Router';
 
+import { mountStoreDevtool } from 'simple-zustand-devtools';
+import { useAuthStore } from './store/auth.store';
+import { useHolidayStore } from './store/holidays.store';
+
+if (process.env.NODE_ENV === 'development') {
+  mountStoreDevtool('Auth', useAuthStore);
+  mountStoreDevtool('Holidays', useHolidayStore);
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <MainAppRouter />

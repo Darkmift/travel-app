64eb259b1c7f2507fff6ecad143cf1c6f5eca3d8
Avi E Koenig <darkmift@gmail.com>
2023-11-08 10:32:37 -0300

    [dockerize] changed related configs

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,4 +1,4 @@
-const ENV = import.meta.env.VITE_NODE_ENV;
-export const BASE_URL = ENV === 'production' ? '/' : import.meta.env.VITE_API_URL;
+export const ENV = import.meta.env.VITE_NODE_ENV;
+export const BASE_URL = import.meta.env.VITE_API_URL;
 export const TOKEN_LS_KEY = 'token';
 export const HOST_URL = import.meta.env.VITE_HOST_URL;
